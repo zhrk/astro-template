69106654307f7f14b30546ba521a4b2023c52838
Zakhar Shchipets <zshchipets@gmail.com>
2024-08-19 00:00:15 +0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
   outDir: "./build",
   scopedStyleStrategy: "class",
-  server: { open: true },
+  server: { open: true, host: true },
   devToolbar: { enabled: false },
 });
