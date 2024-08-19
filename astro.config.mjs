43f217dce539757c183e284ee25d9d0e4d8248aa
zhrk <zshchipets@gmail.com>
2024-08-20 01:03:26 +0400

    use scss modules for styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   outDir: "./build",
-  scopedStyleStrategy: "class",
   server: { open: true, host: true },
   devToolbar: { enabled: false },
 });
